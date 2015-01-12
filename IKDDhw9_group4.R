27a4360ea78cd5ff75fb10130645bf37fb5f90f0
Sean <sean820117@gmail.com>
2015-01-12 21:32:53 +0800

    first edition

---
+++ IKDDhw9_group4.R
@@ -0,0 +1,16 @@
+data<-read.table(url("http://www.datagarage.io/api/5488687d9cbc60e12d300ba5"))
+o<-seq(2, 4000, by=2)
+d_c<-as.character(data[o, ])
+p_m<-sapply(strsplit(d_c[], ""), function(d_c) which(d_c == ":"))
+data<-data.frame(X=as.double(substr(d_c,p_m[2,]+1, 38)), Y=as.double(substr(d_c,p_m[1,]+1, 17)))
+
+x<-data[,1]
+y<-data[,2]
+fitting<-lm(y~poly(x,25,raw=TRUE))
+x_axis<-seq(-10,10, length=50)
+plot(x,y)
+lines(x_axis, predict(fitting, data.frame(x=x_axis)), col="red")
+y_pred = predict(fitting,data.frame(x=x_axis))
+summary(fitting)
+
+write.csv(y_pred, file = "IKDDhw9-y.csv", fileEncoding = "UTF-8")
